fd5cbc1c23217b232a2349dd5928664a9eb4787b
Alice Aquino <53798258+lic3as@users.noreply.github.com>
2024-06-12 00:13:30 -0300

    start

---
+++ desafio02.py
@@ -0,0 +1,5 @@
+dia = input('Qual o dia do seu nascimento?')
+mes = input('Qual o mês?')
+ano = input('E qual o ano?')
+
+print('Você nasceu em ' + dia + '/' + mes + '/' + ano)
---
+++ desafio03.py
@@ -0,0 +1,5 @@
+num1 = int(input('Insira o primeiro número: '))
+num2 = int(input('Insira o segundo número: '))
+soma = num1 + num2
+
+print('A soma de' , num1 , '+' , num2 , 'é' , soma)
